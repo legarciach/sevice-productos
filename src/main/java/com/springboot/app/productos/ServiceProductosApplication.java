78be41f4d9fc449835f190fe652d3c3156a5e4e8
luis.garcia <luis.garcia@rappi.com>
2020-09-25 17:44:18 -0500

    Creacion del proyecto

---
+++ src/main/java/com/springboot/app/productos/ServiceProductosApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.app.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProductosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProductosApplication.class, args);
+    }
+
+}
